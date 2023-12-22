2e83f651bfd70d1ea7817fee1dda22ed416f93b4
Marvin Mesaric <dabbleuw@gmail.com>
2023-12-22 20:47:47 +0100

    created first Column for database

--- mawbot/database.py
+++ mawbot/database.py
@@ -1,1 +1,12 @@
 from mawbot import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    profilePicture = db.Column(db.String(20), nullable=False, default='mawbot.png')
+
+
+
