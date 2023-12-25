fd090d6bdd0d135224e011edf8db1331cb62ea1f
Marvin Mesaric <dabbleuw@gmail.com>
2023-12-25 20:40:43 +0100

    created some database tables

--- mawbot/database.py
+++ mawbot/database.py
@@ -1,5 +1,6 @@
 from mawbot import db
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 
 class User(db.Model):
@@ -12,7 +13,31 @@ class User(db.Model):
 
 class Temperature(db.Model):
     id = db.Column(db.Integer, primary=True)
-    temperature = db.column(db.Float, unique=True, nullable=False)
+    temperature = db.Column(db.Float, unique=True, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
+class Humidity(db.Model):
+    id = db.Column(db.Integer, primary=True)
+    humidity = db.Column(db.Float, unique=True, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
+class SoilMoisture(db.Model):
+    id = db.Column(db.Integer, primary=True)
+    soilMoisture = db.Column(db.Float, unique=True, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
+class Rain(db.Model): # Mit Emanuel Abklären was alles hier übertragen wird.
+    id = db.Column(db.Integer, primary=True)
+    rain = db.Column(db.Boolean, unique=True, nullable=False) 
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Battery(db.Model):
+    id = db.Column(db.Integer, primary=True)
+    status = db.Column(db.Integer, unique=True, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Progress(db.Model):
+    id = db.Column(db.Integer, primary=True)
+    progress = db.Column(db.Integer, unique=True, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
