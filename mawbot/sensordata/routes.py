c7378359975be97677801a2808720ab4f5ddd89c
Marvin Mesaric <dabbleuw@gmail.com>
2024-02-22 23:37:25 +0100

    feat: <Output for sensordata >

--- mawbot/sensordata/routes.py
+++ mawbot/sensordata/routes.py
@@ -1,4 +1,7 @@
-from flask import render_template, Blueprint
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from mawbot import db
+from mawbot.database import *
 
 sensordata = Blueprint('sensordata', __name__)
 
@@ -7,9 +10,16 @@ def controls():
     return render_template("controls.html")
 
 @sensordata.route("/sensordatas")
+@login_required
 def sensordatas():
-    return render_template("sensordatas.html")
+    tempValue = Temperature.query.order_by(Temperature.id.desc()).filter_by(uuid=current_user.uuid).first()
+    humValue = Humidity.query.order_by(Humidity.id.desc()).filter_by(uuid=current_user.uuid).first()
+    soilMValue = SoilMoisture.query.order_by(SoilMoisture.id.desc()).filter_by(uuid=current_user.uuid).first()
+    rainValue = Rain.query.order_by(Rain.id.desc()).filter_by(uuid=current_user.uuid).first()
+    batValue = Battery.query.order_by(Battery.id.desc()).filter_by(uuid=current_user.uuid).first()
+    progValue = Progress.query.order_by(Progress.id.desc()).filter_by(uuid=current_user.uuid).first()
+    return render_template("sensordatas.html", temperatureValue=tempValue.temperature, humidityValue=humValue.humidity, soilMoistureValue=soilMValue.soilMoisture, rainValue=rainValue.rain,  batteryValue=batValue.status, progressValue=progValue.progress)
 
-@sensordata.route("/recievedata")
-def recievedata():
-    
+# @sensordata.route("/recievedata")
+# def recievedata():
+     
